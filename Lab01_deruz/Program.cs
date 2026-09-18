134cb7ad2bf886e3a9ae125daac71dd11a2581ac
Aleksander <deruzkinsanek@gmail.com>
2026-09-18 13:40:05 +0300

    Lab01_deruz task 2: temperature converter

--- Lab01_deruz/Program.cs
+++ Lab01_deruz/Program.cs
@@ -106,14 +106,14 @@
 
 
 
-Console.WriteLine();
-int scholarship = 800;
-int monthlyExpenses = 4800;
-const int MonthsInSemester = 4; 
+// Console.WriteLine();
+// int scholarship = 800;
+// int monthlyExpenses = 4800;
+// const int MonthsInSemester = 4; 
 
-int monthlyMoney = scholarship - monthlyExpenses;
-Console.WriteLine($"Остаток за месяц: {monthlyMoney}");
+// int monthlyMoney = scholarship - monthlyExpenses;
+// Console.WriteLine($"Остаток за месяц: {monthlyMoney}");
 
 
-int semesterMoney = MonthsInSemester * monthlyMoney;
-Console.WriteLine($"Остаток за семестр: {semesterMoney}");
+// int semesterMoney = MonthsInSemester * monthlyMoney;
+// Console.WriteLine($"Остаток за семестр: {semesterMoney}");
---
+++ Lab01_deruz/TempConventer/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Конвертатор температур");
+double celsius = 36.6;
+
+
+double Fahrenheit = celsius * 9.0 / 5.0 + 32.0;
+
+const double kelvinCount = 273.15;
+double kelvin = celsius + kelvinCount;
+
+Console.WriteLine($"{celsius}°C = {Fahrenheit}°F = {kelvin}K");
